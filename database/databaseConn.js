634477303ca3e7f7723163ae737039f35785ff07
lekesmile <lekesmile@hotmail.com>
2020-07-13 21:28:59 +0300

    env

--- database/databaseConn.js
+++ database/databaseConn.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
-const config = require('../config')
-
+const config = require("../config");
 
 //Mongose Conection
 
 try {
-    mongoose.set('useCreateIndex', true)
-    mongoose.set('useFindAndModify', false);
-    mongoose.connect(config.dbcUrl, { useNewUrlParser: true,  useUnifiedTopology: true})
-      .then(()=>{
-         console.log("We're connected to db") 
-      }) 
-    } catch (error) {
-        console.log("Error connecting to the database" + error)
-    }
-
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useFindAndModify", false);
+  mongoose
+    .connect(
+      config.dbcUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      { useMongoClient: true }
+    )
+    .then(() => {
+      console.log("We're connected to db");
+    });
+} catch (error) {
+  console.log("Error connecting to the database" + error);
+}
