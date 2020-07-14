91f32f772a41a912bf4b342f7d7211ec14a76adb
lekesmile <lekesmile@hotmail.com>
2020-07-14 12:20:32 +0300

    update

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ app.use(express.static("public"));
 app.use("/", book);
 app.use("/", user);
 
-// Checking id production
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
+  // import env file
   require("dotenv").config();
   config;
-  app.use(express.static("../build"));
+  // Set static folder
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
--- database/databaseConn.js
+++ database/databaseConn.js
@@ -4,16 +4,13 @@ const secret = require("../config/config");
 //Mongose Conection
 
 try {
-  mongoose.set("useCreateIndex", true);
+  mongoose.set("useNewUrlParser", true);
   mongoose.set("useFindAndModify", false);
-  mongoose
-    .connect(secret.dbcUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("We're connected to Mongodb");
-    });
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useUnifiedTopology", true);
+  mongoose.connect(secret.dbcUrl).then(() => {
+    console.log("We're connected to Mongodb");
+  });
 } catch (error) {
   console.log("Error connecting to the database" + error);
 }
