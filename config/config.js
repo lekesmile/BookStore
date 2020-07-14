8aeedeeee5cd4e8e289277a86f203fd336528f54
lekesmile <lekesmile@hotmail.com>
2020-07-14 17:02:58 +0300

    db

--- config/config.js
+++ config/config.js
@@ -6,6 +6,9 @@ const secrets = {
   PORT: Number(process.env.PORT),
   DB_URL: process.env.DB_URL,
   DBC_URL: process.env.DBC_URL,
+  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_USER: process.env.DB_NAME,
+  MONGODB_PASS: process.env.DB_PASS,
   SALTROUNDS: Number(process.env.SALTROUNDS),
   JWT_SECRET: process.env.JWT_SECRET,
 };
--- database/databaseConn.js
+++ database/databaseConn.js
@@ -1,36 +1,17 @@
 const mongoose = require("mongoose");
 const secret = require("../config/config");
-var mongodb = require("mongodb");
 // //Mongose Conection
 
-const url = secret.DBC_URL;
+const url = encodeURI(secret.DBC_URL);
 
-// try {
-//   mongoose.set("useNewUrlParser", true);
-//   mongoose.set("useFindAndModify", false);
-//   mongoose.set("useCreateIndex", true);
-//   mongoose.set("useUnifiedTopology", true);
-//   mongoose.connect(url).then(() => {
-//     console.log("We're connected to Mongodb");
-//   });
-// } catch (error) {
-//   console.log("Error connecting to the database" + error);
-// }
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, function (
-  err,
-  client
-) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-});
+try {
+  mongoose.set("useNewUrlParser", true);
+  mongoose.set("useFindAndModify", false);
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useUnifiedTopology", true);
+  mongoose.connect(url).then(() => {
+    console.log("We're connected to Mongodb");
+  });
+} catch (error) {
+  console.log("Error connecting to the database" + error);
+}
