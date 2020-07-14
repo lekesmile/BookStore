73b9e2a764f4d32ee2c12699b270df297d5a12c3
lekesmile <lekesmile@hotmail.com>
2020-07-14 17:41:54 +0300

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const book = require("./router/book");
 const user = require("./router/user");
 const path = require("path");
+const secrets = require("./config/config");
 
 const app = express();
 
@@ -25,13 +26,13 @@ if (process.env.NODE_ENV === "production") {
   require("dotenv").config();
   config;
   // Set static folder
-  // app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || secrets.PORT;
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`);
