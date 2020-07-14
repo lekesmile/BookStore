0f5171cf6cafccc0c3b26b9561ca2da54c232c1c
lekesmile <lekesmile@hotmail.com>
2020-07-14 11:14:12 +0300

    modify env

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+require("dotenv").config();
+const config = require("./config/config");
+require("./database/databaseConn");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const book = require("./router/book");
 const user = require("./router/user");
 const path = require("path");
-
-require("./database/databaseConn");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use("/", user);
 // Checking id production
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
+  config;
   app.use(express.static("../build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
--- config/config.js
+++ config/config.js
@@ -4,12 +4,9 @@ const secrets = {
   APP_NAME: "BookStore",
 
   domain: process.env.APP_URL,
-  port: process.env.PORT,
-  env: process.env.APP_ENV,
-
+  port: Number(process.env.PORT),
   dbUrl: process.env.DB_URL,
   dbcUrl: process.env.DBC_URL,
-  dbName: process.env.DB_NAME,
   passwordSaltNo: process.env.SALTROUNDS,
   JWT_Secret: process.env.JWT_SECRET,
 };
