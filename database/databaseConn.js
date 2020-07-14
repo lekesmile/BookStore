b7a654ec5d5f3618dbcfa4803cd6098922a8b343
lekesmile <lekesmile@hotmail.com>
2020-07-14 15:09:55 +0300

    up

--- database/databaseConn.js
+++ database/databaseConn.js
@@ -3,26 +3,14 @@ const secret = require("../config/config");
 
 //Mongose Conection
 
-// try {
-//   mongoose.set("useNewUrlParser", true);
-//   mongoose.set("useFindAndModify", false);
-//   mongoose.set("useCreateIndex", true);
-//   mongoose.set("useUnifiedTopology", true);
-//   mongoose.createConnection(secret.DBC_URL).then(() => {
-//     console.log("We're connected to Mongodb");
-//   });
-// } catch (error) {
-//   console.log("Error connecting to the database" + error);
-// }
-
-const MongoClient = require("mongodb").MongoClient;
-const uri = secret.DBC_URL;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+try {
+  mongoose.set("useNewUrlParser", true);
+  mongoose.set("useFindAndModify", false);
+  mongoose.set("useCreateIndex", true);
+  mongoose.set("useUnifiedTopology", true);
+  mongoose.createConnection(secret.DBC_URL).then(() => {
+    console.log("We're connected to Mongodb");
+  });
+} catch (error) {
+  console.log("Error connecting to the database" + error);
+}
