dae797b269c6b6b269ecc1d3bf106d778a5298d8
lekesmile <lekesmile@hotmail.com>
2020-07-14 14:50:55 +0300

    changed db connection

--- database/databaseConn.js
+++ database/databaseConn.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 const secret = require("../config/config");
-require("dotenv").config();
 
 //Mongose Conection
 
-const url = process.env.DBC_URL;
-try {
-  mongoose.set("useNewUrlParser", true);
-  mongoose.set("useFindAndModify", false);
-  mongoose.set("useCreateIndex", true);
-  mongoose.set("useUnifiedTopology", true);
-  mongoose.connect(url).then(() => {
-    console.log("We're connected to Mongodb");
-  });
-} catch (error) {
-  console.log("Error connecting to the database" + error);
-}
+// try {
+//   mongoose.set("useNewUrlParser", true);
+//   mongoose.set("useFindAndModify", false);
+//   mongoose.set("useCreateIndex", true);
+//   mongoose.set("useUnifiedTopology", true);
+//   mongoose.createConnection(secret.DBC_URL).then(() => {
+//     console.log("We're connected to Mongodb");
+//   });
+// } catch (error) {
+//   console.log("Error connecting to the database" + error);
+// }
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = secret.DBC_URL;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
