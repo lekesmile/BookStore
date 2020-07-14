ededd430b9b8786abcf5be613167eba6f7ce83d4
lekesmile <lekesmile@hotmail.com>
2020-07-14 14:22:37 +0300

    db

--- database/databaseConn.js
+++ database/databaseConn.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const secret = require("../config/config");
+require("dotenv").config();
 
 //Mongose Conection
 
+const url = process.env.DBC_URL;
 try {
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
   mongoose.set("useUnifiedTopology", true);
-  mongoose.connect(secret.DBC_URL).then(() => {
+  mongoose.connect(url).then(() => {
     console.log("We're connected to Mongodb");
   });
 } catch (error) {
