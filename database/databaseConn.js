73b6ea61d0611287e250a77faecea4fbab75f994
lekesmile <lekesmile@hotmail.com>
2020-07-15 16:06:36 +0300

    update

--- database/databaseConn.js
+++ database/databaseConn.js
@@ -4,17 +4,16 @@ const secret = require("../config/config");
 
 const url = secret.DBC_URL;
 const ServerUrl = process.env.MONGODB_URL;
+const LocalUrl = "mongodb://localhost:27017/BookStore";
 
 try {
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
   mongoose.set("useUnifiedTopology", true);
-  mongoose
-    .connect(ServerUrl || "mongodb://localhost:27017/BookStore")
-    .then(() => {
-      console.log("We're connected to Mongodb");
-    });
+  mongoose.connect(ServerUrl).then(() => {
+    console.log("We're connected to Mongodb");
+  });
 } catch (error) {
   console.log("Error connecting to the database" + error);
 }
